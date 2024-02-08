9b91afeb3d4d71b147458406f0c91bdf43c1c3b9
Sean Bikkes <bixkitts@gmail.com>
2024-02-08 20:57:41 +0100

    started on link replication

--- Content.Shared/DeviceLinking/DeviceLinkSinkComponent.cs
+++ Content.Shared/DeviceLinking/DeviceLinkSinkComponent.cs
@@ -1,4 +1,4 @@
-﻿using Robust.Shared.GameStates;
+using Robust.Shared.GameStates;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Set;
 
 namespace Content.Shared.DeviceLinking;
@@ -6,18 +6,19 @@ namespace Content.Shared.DeviceLinking;
 [RegisterComponent]
 [NetworkedComponent] // for interactions. Actual state isn't currently synced.
 [Access(typeof(SharedDeviceLinkSystem))]
+[AutoGenerateComponentState]
 public sealed partial class DeviceLinkSinkComponent : Component
 {
     /// <summary>
     /// The ports this sink has
     /// </summary>
-    [DataField("ports", customTypeSerializer: typeof(PrototypeIdHashSetSerializer<SinkPortPrototype>))]
+    [DataField("ports", customTypeSerializer: typeof(PrototypeIdHashSetSerializer<SinkPortPrototype>)), AutoNetworkedField]
     public HashSet<string>? Ports;
 
     /// <summary>
     /// Used for removing a sink from all linked sources when it gets removed
     /// </summary>
-    [DataField("links")]
+    [DataField("links"), AutoNetworkedField]
     public HashSet<EntityUid> LinkedSources = new();
 
     /// <summary>
@@ -25,14 +26,14 @@ public sealed partial class DeviceLinkSinkComponent : Component
     /// The counter is counted down by one every tick if it's higher than 0
     /// This is for preventing infinite loops
     /// </summary>
-    [DataField("invokeCounter")]
+    [DataField("invokeCounter"), AutoNetworkedField]
     public int InvokeCounter;
 
     /// <summary>
     /// How high the invoke counter is allowed to get before the links to the sink are removed and the DeviceLinkOverloadedEvent gets raised
     /// If the invoke limit is smaller than 1 the sink can't overload
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite)]
+    [ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
     [DataField("invokeLimit")]
     public int InvokeLimit = 10;
 }
